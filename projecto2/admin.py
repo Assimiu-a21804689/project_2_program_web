55aebed06ceedfbabcc8004a91fcce4683ac7a35
Assimiu-a21804689 <assimium2@gmail.com>
2021-06-24 02:27:10 +0100

    V2

---
+++ projecto2/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Encomendar
+from .models import Sobre
+# Register your models here.
+
+admin.site.register(Encomendar)
+admin.site.register(Sobre)
